9abc0da8da4cb191a5b523b297980100437eca5e
Sharashena Chairani <shenachairani@gmail.com>
2022-06-23 23:16:07 +0700

    refinement data and search

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -4,7 +4,7 @@ import { DataGrid } from "@mui/x-data-grid";
 import { Link } from "react-router-dom";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import AddRoundedIcon from "@mui/icons-material/AddRounded";
-// import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import ConfirmDialog from "../confirmDialog/ConfrmDialog";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -47,7 +47,7 @@ const Datatable = ({ rows, columns, handleDelete }) => {
             if (inputSearch) {
                 updatedList = updatedList.filter(
                     (item) =>
-                        setDataPage(item.culturename, item.name, item.username)
+                        setDataPage(item.name, item.name, item.username)
                             .toLowerCase()
                             .search(inputSearch.toLowerCase().trim()) !== -1
                 );
@@ -58,8 +58,6 @@ const Datatable = ({ rows, columns, handleDelete }) => {
         };
         applyFilters();
     }, [inputSearch, province, year, rows]);
-
-    console.log(tahun(2012, 2021));
 
     const actionColumn = [
         {
@@ -75,8 +73,8 @@ const Datatable = ({ rows, columns, handleDelete }) => {
                             style={{ textDecoration: "none" }}
                         >
                             <div className="viewButton">
-                                <EditIcon className="icon" />
-                                </div>
+                            <EditIcon className="icon" />
+                            </div>
                         </Link>
                         <div
                             className="deleteButton"
@@ -114,6 +112,7 @@ const Datatable = ({ rows, columns, handleDelete }) => {
                         <div className="filter-container">
                             {/* <div className="filter-title">
                                 <FilterAltIcon />
+                                Filter :
                             </div> */}
                             <CustomFilter
                                 options={provinces}
@@ -122,7 +121,7 @@ const Datatable = ({ rows, columns, handleDelete }) => {
                                 setValue={setProvince}
                             />
                             <CustomFilter
-                                options={tahun(2012, 2021)}
+                                options={tahun(2010, 2023)}
                                 label="Tahun"
                                 value={year}
                                 setValue={setYear}
@@ -155,7 +154,8 @@ const Datatable = ({ rows, columns, handleDelete }) => {
                 rows={list}
                 columns={columns.concat(actionColumn)}
                 pageSize={9}
-                rowsPerPageOptions={[9]}
+                rowsPerPageOptions={[9, 18, 20, 100]}
+                paginationMode="client"
                 checkboxSelection
             />
             <ConfirmDialog
@@ -169,3 +169,177 @@ const Datatable = ({ rows, columns, handleDelete }) => {
 };
 
 export default Datatable;
+
+// import React, { useEffect, useState } from "react";
+// import "./datatable.scss";
+// import { DataGrid } from "@mui/x-data-grid";
+// import { Link } from "react-router-dom";
+// import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+// import AddRoundedIcon from "@mui/icons-material/AddRounded";
+// // import FilterAltIcon from "@mui/icons-material/FilterAlt";
+// import ConfirmDialog from "../confirmDialog/ConfrmDialog";
+// import EditIcon from '@mui/icons-material/Edit';
+// import DeleteIcon from '@mui/icons-material/Delete';
+// import { fieldName, pathName, setDataPage, tahun } from "../../utils/naming";
+// import CustomFilter from "../fields/customFilter/CustomFilter";
+// import { getProvinces } from "../../redux/apiCalls";
+// import { useDispatch, useSelector } from "react-redux";
+
+// const Datatable = ({ rows, columns, handleDelete }) => {
+//     const [inputSearch, setInputSearch] = useState("");
+//     const [list, setList] = useState([]);
+//     const [openConfirm, setOpenConfirm] = useState(false);
+//     const [deleteVal, setDeleteVal] = useState({});
+//     const [province, setProvince] = useState("");
+//     const [year, setYear] = useState("");
+
+//     const dispatch = useDispatch();
+//     const { provinces } = useSelector((state) => state.provinces);
+
+//     useEffect(() => {
+//         getProvinces(dispatch);
+//     }, []);
+
+//     useEffect(() => {
+//         setList(rows);
+//     }, [rows]);
+
+    
+//     useEffect(() => {
+//         const applyFilters = () => {
+//             let updatedList = rows;
+            
+//             if (year) {
+//                 updatedList = updatedList.filter((item) => item.year === year);
+//             }
+//             if (province) {
+//                 updatedList = updatedList.filter(
+//                     (item) => item.province._id === province
+//                 );
+                
+//             }
+//             if (inputSearch) {
+//                 updatedList = updatedList.filter(
+//                     (item) =>
+//                         setDataPage(item.culturename, item.name, item.username)
+//                             .toLowerCase()
+//                             .search(inputSearch.toLowerCase().trim()) !== -1
+//                 );
+//             }
+
+//             console.log(updatedList);
+//             setList(updatedList);
+//         };
+//         applyFilters();
+//     }, [inputSearch, province, year, rows]);
+
+//     console.log(tahun(2010, 2023));
+
+//     const actionColumn = [
+//         {
+//             field: "action",
+//             headerName: "Action",
+//             minWidth: 100,
+//             flex: 1,
+//             renderCell: (params) => {
+//                 return (
+//                     <div className="cellAction">
+//                         <Link
+//                             to={`./${params.row._id}`}
+//                             style={{ textDecoration: "none" }}
+//                         >
+//                             <div className="viewButton">
+//                                 <EditIcon className="icon" />
+//                                 </div>
+//                         </Link>
+//                         <div
+//                             className="deleteButton"
+//                             onClick={() => {
+//                                 setOpenConfirm(true);
+//                                 setDeleteVal(fieldName(params.row));
+//                             }}
+//                         >
+//                             <DeleteIcon className="icon" />
+//                         </div>
+//                     </div>
+//                 );
+//             },
+//         },
+//     ];
+//     return (
+//         <div className="datatable">
+//             <div className="top">
+//                 <div className="searchFilters">
+//                     <div className="search">
+//                         <input
+//                             type="text"
+//                             placeholder="Search..."
+//                             value={inputSearch}
+//                             onChange={(e) => setInputSearch(e.target.value)}
+//                             onKeyPress={(event) => {
+//                                 if (event.key === "Enter") {
+//                                     setInputSearch(event.target.value);
+//                                 }
+//                             }}
+//                         />
+//                         <SearchOutlinedIcon />
+//                     </div>
+//                     {window.location.pathname === "/cultures" && (
+//                         <div className="filter-container">
+//                             {/* <div className="filter-title">
+//                                 <FilterAltIcon />
+//                             </div> */}
+//                             <CustomFilter
+//                                 options={provinces}
+//                                 label="Provinsi"
+//                                 value={province}
+//                                 setValue={setProvince}
+//                             />
+//                             <CustomFilter
+//                                 options={tahun(2010, 2023)}
+//                                 label="Tahun"
+//                                 value={year}
+//                                 setValue={setYear}
+//                             />
+//                             {(province || year) && (
+//                                 <div
+//                                     className="reset"
+//                                     onClick={() => {
+//                                         setProvince("");
+//                                         setYear("");
+//                                     }}
+//                                 >
+//                                     Reset filter
+//                                 </div>
+//                             )}
+//                         </div>
+//                     )}
+//                 </div>
+
+//                 <Link to={`./new`} style={{ textDecoration: "none" }}>
+//                     <div className="addButton">
+//                         <AddRoundedIcon />
+//                         Tambah {pathName()}
+//                     </div>
+//                 </Link>
+//             </div>
+//             <DataGrid
+//                 className="datagrid"
+//                 getRowId={(row) => row._id}
+//                 rows={list}
+//                 columns={columns.concat(actionColumn)}
+//                 pageSize={9}
+//                 rowsPerPageOptions={[9]}
+//                 checkboxSelection
+//             />
+//             <ConfirmDialog
+//                 open={openConfirm}
+//                 setOpen={setOpenConfirm}
+//                 deleteVal={deleteVal}
+//                 handleDelete={handleDelete}
+//             />
+//         </div>
+//     );
+// };
+
+// export default Datatable;
--- src/pages/listCultures.jsx/ListCultures.jsx
+++ src/pages/listCultures.jsx/ListCultures.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Datatable from "../../components/datatable/Datatable";
 import { deleteCulture, getCultures } from "../../redux/apiCalls";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
+import CircularProgress from "@mui/material/CircularProgress";
 import "react-toastify/dist/ReactToastify.css";
 import "./listcultures.scss";
 
 const ListCultures = () => {
     const dispatch = useDispatch();
-    const { cultures } = useSelector((state) => state.cultures);
+    const { cultures, isFetching } = useSelector((state) => state.cultures);
 
     useEffect(() => {
         getCultures(dispatch);
-    }, []);
+    }, [dispatch]);
 
     const handleDelete = (id) => {
-        deleteCulture(id, dispatch, toast);
+        deleteCulture(id, dispatch, toast, cultures);
     };
 
-    const provinceColumns = [
+    const cultureColumns = [
         { field: "_id", headerName: "ID", minWidth: 100, flex: 1 },
         {
             field: "culturename",
@@ -95,24 +96,148 @@ const ListCultures = () => {
 
     return (
         <div className="listCultures">
-            <Datatable
-                rows={cultures}
-                columns={provinceColumns}
-                handleDelete={handleDelete}
-            />
-            <ToastContainer
-                position="bottom-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick={false}
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
+            {isFetching ? (
+                <CircularProgress />
+            ) : (
+                <>
+                    <Datatable
+                        rows={cultures}
+                        columns={cultureColumns}
+                        handleDelete={handleDelete}
+                    />
+                    <ToastContainer
+                        position="bottom-center"
+                        autoClose={5000}
+                        hideProgressBar={false}
+                        newestOnTop={false}
+                        closeOnClick={false}
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                    />
+                </>
+            )}
         </div>
     );
 };
 
 export default ListCultures;
+// import React, { useEffect } from "react";
+// import Datatable from "../../components/datatable/Datatable";
+// import { deleteCulture, getCultures } from "../../redux/apiCalls";
+// import { useDispatch, useSelector } from "react-redux";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import "./listcultures.scss";
+
+// const ListCultures = () => {
+//     const dispatch = useDispatch();
+//     const { cultures } = useSelector((state) => state.cultures);
+
+//     useEffect(() => {
+//         getCultures(dispatch);
+//     }, []);
+
+//     const handleDelete = (id) => {
+//         deleteCulture(id, dispatch, toast);
+//     };
+
+//     const provinceColumns = [
+//         { field: "_id", headerName: "ID", minWidth: 100, flex: 1 },
+//         {
+//             field: "culturename",
+//             headerName: "Nama Tari",
+//             minWidth: 150,
+//             flex: 1,
+
+//             renderCell: (params) => {
+//                 return params.row.name;
+//             },
+//         },
+//         {
+//             field: "province",
+//             headerName: "Provinsi",
+//             minWidth: 150,
+//             flex: 1,
+//             renderCell: (params) => {
+//                 return params.row.province.name;
+//             },
+//         },
+//         {
+//             field: "year",
+//             headerName: "Tahun",
+//             minWidth: 60,
+//             flex: 1,
+//             renderCell: (params) => {
+//                 return params.row.year || "-";
+//             },
+//         },
+//         {
+//             field: "desc",
+//             headerName: "Deskripsi",
+//             minWidth: 150,
+//             flex: 1,
+//             renderCell: (params) => {
+//                 return params.row.desc || "-";
+//             },
+//         },
+//         // {
+//         //     field: "reg_num",
+//         //     headerName: "No. Regist",
+//         //     minWidth: 100,
+//         //     flex: 1,
+
+//         //     renderCell: (params) => {
+//         //         return params.row.reg_num || "-";
+//         //     },
+//         // },
+//         // {
+//         //     field: "imgs",
+//         //     headerName: "Foto",
+//         //     minWidth: 50,
+//         //     flex: 1,
+
+//         //     renderCell: (params) => {
+//         //         return params.row.imgs.length > 0
+//         //             ? params.row.imgs.length
+//         //             : "-";
+//         //     },
+//         // },
+//         // {
+//         //     field: "videos",
+//         //     headerName: "Video",
+//         //     minWidth: 50,
+//         //     flex: 1,
+
+//         //     renderCell: (params) => {
+//         //         return params.row.videos.length > 0
+//         //             ? params.row.videos.length
+//         //             : "-";
+//         //     },
+//         // },
+//     ];
+
+//     return (
+//         <div className="listCultures">
+//             <Datatable
+//                 rows={cultures}
+//                 columns={provinceColumns}
+//                 handleDelete={handleDelete}
+//             />
+//             <ToastContainer
+//                 position="bottom-center"
+//                 autoClose={5000}
+//                 hideProgressBar={false}
+//                 newestOnTop={false}
+//                 closeOnClick={false}
+//                 rtl={false}
+//                 pauseOnFocusLoss
+//                 draggable
+//                 pauseOnHover
+//             />
+//         </div>
+//     );
+// };
+
+// export default ListCultures;
