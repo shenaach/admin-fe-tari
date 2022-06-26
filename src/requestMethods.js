c3dd7434c05e5ad12ae103d08f97778cc7fccd01
Sharashena Chairani <shenachairani@gmail.com>
2022-06-26 23:22:18 +0700

    add heroku api link

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,19 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
-// const TOKEN =
-//     JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-//         .currentUser === null
-//         ? ""
-//         : JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-//               .currentUser.accessToken;
-// const TOKEN =
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjdkNWZkMDM0MjMyM2ZmODlhZDFjNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNTI1NTgzNiwiZXhwIjoxNjM1NTE1MDM2fQ.azxlE9NUbxrwXzbaR22IzVZN-CGAGnV6mNsL2ig6B3o";
-
-// console.log(
-//     JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-//         .currentUser.accessToken
-// );
+const BASE_URL = "https://maps-tari.herokuapp.com/api/";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
